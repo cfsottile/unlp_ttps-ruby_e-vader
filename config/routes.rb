37071e95c9458180b9461d7430d5413bbab51ef9
Cristian Sottile <cfsottile@gmail.com>
2015-12-18 19:47:10 -0300

    scaffolded the resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  root 'page#home'
+
+  scope format: false do
+    get 'page/home'
+
+    resources :invoices
+    resources :people
+    resources :contacts
+    resources :clients
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
