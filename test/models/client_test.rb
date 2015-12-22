19cfc47f106090fa477f609d7949f6469fcf52c9
Cristian Sottile <cfsottile@gmail.com>
2015-12-21 23:37:41 -0300

    fixed tests

--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -1,23 +1,21 @@
 require 'test_helper'
 
 class ClientTest < ActiveSupport::TestCase
-  # def setup
-  #   @good_client = client(:one)
-  #   @bad_client = client(:two)
-  # end
-
   test "new client without contact data" do
-    client = clients(:four)
+    client = new_client_with_document_number("90100210")
     assert(client.save, "client should be saved")
   end
 
   test "new client with contact data" do
-    client = clients(:four)
-    contact = contacts(:four)
-    client.contacts<<contact
+    client = new_client_with_document_number("90100210")
+    contact = client.contacts.new(
+      kind: "email",
+      value: "ada_lovelace@arnet.com.ar"
+    )
 
     assert(client.save, "client should be saved")
 
+    client.reload
     saved_contact = client.contacts.first
     refute_nil(saved_contact, "contact should be saved")
     assert_equal(saved_contact.kind, contact.kind, "contact was not correctly saved")
@@ -25,30 +23,38 @@ class ClientTest < ActiveSupport::TestCase
   end
 
   test "new client with an invalid document number" do
-    client = clients(:five)
+    client = new_client_with_document_number("document")
     refute(client.save, "client should not be saved")
   end
 
   test "delete client who has invoices" do
     client = clients(:four)
-    client.save
-    invoices.each { |i| i.save }
-
-    client_invoices = client.invoices
+    client_invoices = client.invoices.to_a
     client.destroy
 
-    assert_nil(Client.find_by(document_number: "90100203"), "client should be deleted")
+    assert(client.destroyed?, "client should be deleted")
 
     client_invoices.each do |i|
-      assert_nil(Invoice.find(i.id), "invoices should be deleted")
+      assert(i.destroyed?, "invoices should be deleted")
     end
   end
 
   test "delete client who has no invoices" do
     client = clients(:six)
-    client.save
     client.destroy
 
-    assert_nil(Client.find_by(document_number: "90100205"), "client should be deleted")
+    assert(client.destroyed?, "client should be deleted")
   end
+
+  private
+    def new_client_with_document_number(document_number)
+      Client.new(
+        first_name: "Ada",
+        last_name: "Lovelace",
+        document_number: document_number,
+        gender: "female",
+        birthday: Date.new(1815, 12, 10),
+        cuilt: "20-90100210-5"
+      )
+    end
 end
